8acf253fffc3bbe3c3a150e28dbb15b65cac49dc
Nikhil Agarwal <thenikiscooking@gmail.com>
2018-11-02 23:28:15 -0700

    initial homepage

--- mahalo_me/lib/screens/Home/index.dart
+++ mahalo_me/lib/screens/Home/index.dart
@@ -1,18 +1,82 @@
 import "package:flutter/material.dart";
 
 class HomeScreen extends StatefulWidget {
-const HomeScreen({ Key key }) : super(key: key);
+  const HomeScreen({Key key}) : super(key: key);
 
-@override
-HomeScreenState createState() => new HomeScreenState();
-
+  @override
+  HomeScreenState createState() => new HomeScreenState();
 }
 
-class HomeScreenState extends State<HomeScreen>{
-  
+class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(body:new Center(child: new Text("Home"),));
+    return new MaterialApp(
+        home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.call_made), text: "Send"),
+              Tab(icon: Icon(Icons.call_received), text: "Receive"),
+              Tab(icon: Icon(Icons.attach_money), text: "Reload"),
+            ],
+          ),
+          title: Text('MahaloMe'),
+          centerTitle: true,
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the Drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('John Appleseed'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Settings'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Reports'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Logout'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
