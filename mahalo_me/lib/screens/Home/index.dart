70c57194c1590d7ab06526d15fae6e76eb1e9e28
Nikhil Agarwal <thenikiscooking@gmail.com>
2018-11-03 01:08:46 -0700

    UI for Drawer

--- mahalo_me/lib/screens/Home/index.dart
+++ mahalo_me/lib/screens/Home/index.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "../../theme/style.dart" as Theme;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -10,16 +11,16 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: DefaultTabController(
+    return new DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Theme.ThemeColors.cyan,
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.call_made), text: "Send"),
-              Tab(icon: Icon(Icons.call_received), text: "Receive"),
-              Tab(icon: Icon(Icons.attach_money), text: "Reload"),
+              Tab(text: "Send"),
+              Tab(text: "Receive"),
+              Tab(text: "Reload"),
             ],
           ),
           title: Text('MahaloMe'),
@@ -40,13 +41,48 @@ class HomeScreenState extends State<HomeScreen> {
             // Important: Remove any padding from the ListView.
             padding: EdgeInsets.zero,
             children: <Widget>[
-              DrawerHeader(
-                child: Text('John Appleseed'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              new Container(
+                height: 125.0,
+                child: DrawerHeader(
+                  padding: EdgeInsets.all(18.0),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('John Appleseed',
+                          style: new TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold)),
+                      Text('johnappleseed@gmail.com',
+                          style: new TextStyle(color: Colors.white))
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.ThemeColors.cyan,
+                  ),
                 ),
               ),
+              new Container(
+                  height: 40.0,
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Text(
+                        "\$50.00",
+                        style: new TextStyle(
+                            fontSize: 15.0, color: new Color(0xFF4d4d4d)),
+                      ),
+                      new SizedBox(width: 15.0, height: 0.0),
+                      new Text("Balance",
+                          style: new TextStyle(fontSize: 15.0, color: new Color(0xFF4d4d4d))),
+                    ],
+                  )),
+              Divider(
+                color: Colors.grey,
+              ),
               ListTile(
+                leading: Icon(Icons.settings),
                 title: Text('Settings'),
                 onTap: () {
                   // Update the state of the app
@@ -56,6 +92,7 @@ class HomeScreenState extends State<HomeScreen> {
                 },
               ),
               ListTile(
+                leading: Icon(Icons.equalizer),
                 title: Text('Reports'),
                 onTap: () {
                   // Update the state of the app
@@ -65,6 +102,7 @@ class HomeScreenState extends State<HomeScreen> {
                 },
               ),
               ListTile(
+                leading: Icon(Icons.lock),
                 title: Text('Logout'),
                 onTap: () {
                   // Update the state of the app
@@ -77,6 +115,6 @@ class HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-    ));
+    );
   }
 }
