2f4e6ddf343a9df5811593d7b5ca7e76d276695c
NikAgs <thenikiscooking@gmail.com>
2018-12-15 15:10:46 -0800

    added express screen

---
+++ mahalo_me/lib/screens/Payment/express.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+import '../../theme/style.dart' as Theme;
+
+String selectedUrl =
+    'https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://google.com&client_id=ca_EA3GzzP3l9Go4zdwb2vu0f5mQeT6FZkT';
+
+class ExpressScreen extends StatefulWidget {
+  const ExpressScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ExpressScreenState createState() => _ExpressScreenState();
+}
+
+class _ExpressScreenState extends State<ExpressScreen> {
+  // Instance of WebView plugin
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  // On destroy stream
+  StreamSubscription _onDestroy;
+
+  // On urlChanged stream
+  StreamSubscription<String> _onUrlChanged;
+
+  // On urlChanged stream
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
+
+  StreamSubscription<WebViewHttpError> _onHttpError;
+
+  StreamSubscription<double> _onScrollYChanged;
+
+  StreamSubscription<double> _onScrollXChanged;
+
+  final _urlCtrl = TextEditingController(text: selectedUrl);
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final _history = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebViewPlugin.close();
+
+    _urlCtrl.addListener(() {
+      selectedUrl = _urlCtrl.text;
+    });
+
+    // Add a listener to on destroy WebView, so you can make came actions.
+    _onDestroy = flutterWebViewPlugin.onDestroy.listen((_) {
+      if (mounted) {
+        // Actions like show a info toast.
+        _scaffoldKey.currentState.showSnackBar(
+            const SnackBar(content: const Text('Webview Destroyed')));
+      }
+    });
+
+    // Add a listener to on url changed
+    _onUrlChanged = flutterWebViewPlugin.onUrlChanged.listen((String url) {
+      if (mounted) {
+        setState(() {
+          _history.add('onUrlChanged: $url');
+        });
+      }
+    });
+
+    _onScrollYChanged =
+        flutterWebViewPlugin.onScrollYChanged.listen((double y) {
+      if (mounted) {
+        setState(() {
+          _history.add('Scroll in Y Direction: $y');
+        });
+      }
+    });
+
+    _onScrollXChanged =
+        flutterWebViewPlugin.onScrollXChanged.listen((double x) {
+      if (mounted) {
+        setState(() {
+          _history.add('Scroll in X Direction: $x');
+        });
+      }
+    });
+
+    _onStateChanged =
+        flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      if (mounted) {
+        setState(() {
+          _history.add('onStateChanged: ${state.type} ${state.url}');
+        });
+      }
+    });
+
+    _onHttpError =
+        flutterWebViewPlugin.onHttpError.listen((WebViewHttpError error) {
+      if (mounted) {
+        setState(() {
+          _history.add('onHttpError: ${error.code} ${error.url}');
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Every listener should be canceled, the same should be done with this stream.
+    _onDestroy.cancel();
+    _onUrlChanged.cancel();
+    _onStateChanged.cancel();
+    _onHttpError.cancel();
+    _onScrollXChanged.cancel();
+    _onScrollYChanged.cancel();
+
+    flutterWebViewPlugin.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+        url: selectedUrl,
+        appBar: AppBar(
+          title: const Text('Bank Information'),
+          backgroundColor: Theme.ThemeColors.cyan,
+        ),
+        withLocalStorage: true,
+        hidden: true,
+        initialChild: Theme.loader);
+  }
+}
